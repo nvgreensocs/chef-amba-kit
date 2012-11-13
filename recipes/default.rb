133c6374604b368b7f8dc9ef598f9e9b7bd57c62
Mark Burton <mark@greensocs.com>
2012-11-13 21:53:54 +0100

    stuff

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,17 @@
 
-Chef::Log.warn("Assuming you have downloaded the amba_kit to the chef-amba-kit/files/default directory")
+Chef::Log.warn("Assuming you have downloaded the amba kit amba_socket-1.0.15.tgz to the chef-amba-kit/files/default directory")
 
-cookbook_file "/tmp/amba_socket-1.0.15.tgz" do
+cookbook_file "/vagrant/ModelLibrary/amba_socket-1.0.15.tgz" do
   source "amba_socket-1.0.15.tgz"
   mode "0644"
+end
+
+
+bash "Extract amba kit" do
+  code <<-EOH
+    cd /vagrant/ModelLibrary
+    tar -zxf amba_socket-1.0.15.tgz
+  EOH
+  creates "/vagrant/ModelLibrary/amba_socket"
 end
 
