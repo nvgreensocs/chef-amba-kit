8a3694bc5be52af72c43173b34824833c93b58c7
Mark Burton <mark@greensocs.com>
2012-11-13 15:51:37 +0100

    Add amba kit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,8 @@
-package "amba-kit"
+
+Chef::Log.warn("Assuming you have downloaded the amba_kit to the chef-amba-kit/files/default directory")
+
+cookbook_file "/tmp/amba_socket-1.0.15.tgz" do
+  source "amba_socket-1.0.15.tgz"
+  mode "0644"
+end
+
