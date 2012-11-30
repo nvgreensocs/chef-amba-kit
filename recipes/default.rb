503a2906982a269d153c0076bdf7ce80ece619b5
Mark Burton <mark@greensocs.com>
2012-11-30 13:56:47 +0100

    update amba recipy

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,28 @@
 
+#  -------    CHEF-AMBA --------
+
+# LICENSETEXT
+# 
+#   Copyright (C) 2007 : GreenSocs Ltd
+#       http://www.greensocs.com/ , email: info@greensocs.com
+# 
+# The contents of this file are subject to the licensing terms specified
+# in the file LICENSE. Please consult this file for restrictions and
+# limitations that may apply.
+# 
+# ENDLICENSETEXT
+
+puts "HERE"
+      if File.exist?("./cookbooks/chef-amba-kit/files/default/amba_socket-1.0.15.tgz")
+#  puts "Ok, we're good to go"
+      else
+	puts "Please visit : "
+	puts "https://portal.carbondesignsystems.com/login.aspx?ReturnUrl=%2fModel%2fCarbon%2fTLM-2.0-AMBA"
+	puts "and place the amba kit download in:"
+	puts "cookbooks/chef-amba-kit/files/default/amba_socket-1.0.15.tgz"
+	exit
+      end
+
 Chef::Log.warn("Assuming you have downloaded the amba kit amba_socket-1.0.15.tgz to the chef-amba-kit/files/default directory")
 
 #the plan is to have the recipy simply stall and wait for the file to 'arrive' in the right place 
