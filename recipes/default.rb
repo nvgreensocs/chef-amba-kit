34197f61f3fb6d54c07ac0eb36cf7d122f671cb3
Mark Burton <mark@greensocs.com>
2013-02-05 17:27:16 +0100

    Addig node[:prefix]

--- recipes/default.rb
+++ recipes/default.rb
@@ -29,7 +29,7 @@ Chef::Log.warn("Assuming you have downloaded the amba kit amba_socket-1.0.15.tgz
 #the plan is to have the recipy simply stall and wait for the file to 'arrive' in the right place 
 #constantly pining the user and saying " please put systemc in this location ....
 
-directory "/vagrant/ModelLibrary" do
+directory "#{node[:prefix]}/ModelLibrary" do
   recursive true
   action :create
 end
@@ -42,10 +42,10 @@ end
 
 bash "Extract amba kit" do
   code <<-EOH
-    cd /vagrant/ModelLibrary
+    cd #{node[:prefix]}/ModelLibrary
     tar -zxf #{Chef::Config[:file_cache_path]}/amba_socket-1.0.15.tgz
     rm -rf amba_socket/dependencies/greensocket
   EOH
-  creates "/vagrant/ModelLibrary/amba_socket"
+  creates "#{node[:prefix]}/ModelLibrary/amba_socket"
 end
 
